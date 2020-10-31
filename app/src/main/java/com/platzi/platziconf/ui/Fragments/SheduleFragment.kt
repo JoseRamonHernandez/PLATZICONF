8a9a99edddb5cb9c58ea42bff96d7a9d50d7d78f
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-10-30 23:30:39 -0600

    Video 4_11
    AppBarLayout

--- app/src/main/java/com/platzi/platziconf/ui/Fragments/SheduleFragment.kt
+++ app/src/main/java/com/platzi/platziconf/ui/Fragments/SheduleFragment.kt
@@ -35,7 +35,7 @@ class SheduleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shedule, container, false)
+        return inflater.inflate(R.layout.fragment_schedule, container, false)
     }
 
     companion object {
