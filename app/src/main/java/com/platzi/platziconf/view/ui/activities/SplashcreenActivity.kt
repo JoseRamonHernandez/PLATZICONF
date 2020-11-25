5c90749eaef2f2939ea311c2db1e31864e62e0e7
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-25 04:45:07 -0600

    video 9_1

--- app/src/main/java/com/platzi/platziconf/view/ui/activities/SplashcreenActivity.kt
+++ app/src/main/java/com/platzi/platziconf/view/ui/activities/SplashcreenActivity.kt
@@ -1,10 +1,15 @@
 package com.platzi.platziconf.view.ui.activities
 
+import android.animation.Animator
+import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.platzi.platziconf.R
+import kotlinx.android.synthetic.main.activity_splashcreen.*
 
 class SplashcreenActivity : AppCompatActivity() {
 
@@ -13,9 +18,27 @@ class SplashcreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splashcreen)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+       val animacion = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        ivLogoPlatziConf.startAnimation(animacion)
+
+        val intent = Intent( this, MainActivity::class.java)
+
+        animacion.setAnimationListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+        startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+        } {
+            
+        })
     }
 }
